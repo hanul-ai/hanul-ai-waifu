c12dbc02b99a6d289df05eb7f113f8e6ebce81ad
SB <77054901+SB248@users.noreply.github.com>
2022-09-19 12:45:48 +0900

    TEST Post에 zip 관련 코드 추가

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+from Tools.scripts.pindent import start
 from flask import Flask, request
+import base64
+import zlib
+from Waifu2x import main
+
 
 app = Flask(__name__)
 
@@ -6,8 +11,14 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def post_echo_call():
     params = request.get_json()
+    image_zip = base64.a85encode(params['image'].decode('utf-8'))
+    image = zlib.decompress(image_zip).decode('utf-8')
 
-    return 'ok'
+    result_img = main.Start_Waifu(image)
+
+    base85_zip = zlib.compress(result_img)
+    base85_string = base64.b85encode(base85_zip).decode('utf-8')
+    return base85_string
 
 if __name__ == "__main__":
     app.run()
